62ba8f9a988c07a8cd50fb414beb152c1e52d14f
Alexandre Bernardes <abernardes11192@gmail.com>
2023-05-04 16:44:32 -0300

    :sparkles: Adicionando header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,7 @@
 export default function Header() {
   return (
-    <header>Sistema Solar</header>
+    <header className=" w-screen h-[150px] flex fixed top-0 items-center justify-center bg-gray-6">
+      <h1 className=" text-white">Sistema Solar</h1>
+    </header>
   )
 }
